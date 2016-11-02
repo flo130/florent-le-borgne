1c8e6054833fa400b43dae113c7eb180632be492
exp_of <exp_of@t460s-013.domaine.local>
2016-11-02 15:32:55 +0100

    Mise a jour du script d'install pour mettre à jour la BDD

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# recuperation de composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
+# installation des elements de Symfony
 php composer.phar install
 
+# suppression du fichier Composer
+rm composer.phar
+
+# creation de la bae de données
+php bin/console doctrine:migrations:migrate --no-interaction
