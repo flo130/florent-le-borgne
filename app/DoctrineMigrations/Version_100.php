7e7e289bbb32f19c84aa9cf3423a11fe30312c1c
exp_of <exp_of@172.16.16.22>
2017-02-22 18:41:05 +0100

    script de creation de base

--- app/DoctrineMigrations/Version_100.php
+++ app/DoctrineMigrations/Version_100.php
@@ -2,14 +2,37 @@
 
 namespace Application\Migrations;
 
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use AppBundle\Entity\User;
+use AppBundle\Entity\Category;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Script d'installation de la BDD en version 1.0.0
  */
-class Version20170215164952 extends AbstractMigration
+class Version_100 extends AbstractMigration implements ContainerAwareInterface
 {
+    /**
+     * Container Symfony
+     *
+     * @var ContainerInterface
+     */
+    private $container;
+
+
+    /**
+     * Container Symfony
+     *
+     * @param ContainerInterface $container
+     * @return void
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
     /**
      * @param Schema $schema
      */
@@ -21,7 +44,7 @@ class Version20170215164952 extends AbstractMigration
         $this->addSql('CREATE TABLE ext_log_entries (id INT AUTO_INCREMENT NOT NULL, action VARCHAR(8) NOT NULL, logged_at DATETIME NOT NULL, object_id VARCHAR(64) DEFAULT NULL, object_class VARCHAR(255) NOT NULL, version INT NOT NULL, data LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', username VARCHAR(255) DEFAULT NULL, INDEX log_class_lookup_idx (object_class), INDEX log_date_lookup_idx (logged_at), INDEX log_user_lookup_idx (username), INDEX log_version_lookup_idx (object_id, object_class, version), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE ext_translations (id INT AUTO_INCREMENT NOT NULL, locale VARCHAR(8) NOT NULL, object_class VARCHAR(255) NOT NULL, field VARCHAR(32) NOT NULL, foreign_key VARCHAR(64) NOT NULL, content LONGTEXT DEFAULT NULL, INDEX translations_lookup_idx (locale, object_class, foreign_key), UNIQUE INDEX lookup_unique_idx (locale, object_class, field, foreign_key), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE article_comment (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, article_id INT NOT NULL, article_comment LONGTEXT NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_79A616DBA76ED395 (user_id), INDEX IDX_79A616DB7294869C (article_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\', name VARCHAR(255) NOT NULL, avatar VARCHAR(255) DEFAULT NULL, login_count INT DEFAULT 0 NOT NULL, first_login DATETIME DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_8D93D6495E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\', name VARCHAR(255) NOT NULL, avatar VARCHAR(255) DEFAULT NULL, login_count INT DEFAULT 0 NOT NULL, first_login DATETIME DEFAULT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, slug VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_8D93D6495E237E06 (name), UNIQUE INDEX UNIQ_8D93D649989D9B62 (slug), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, category_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, summary LONGTEXT NOT NULL, article LONGTEXT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, published_at DATETIME DEFAULT NULL, status TINYINT(1) NOT NULL, slug VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_23A0E66989D9B62 (slug), INDEX IDX_23A0E66A76ED395 (user_id), INDEX IDX_23A0E6612469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, root_id INT DEFAULT NULL, parent_id INT DEFAULT NULL, title VARCHAR(64) NOT NULL, slug VARCHAR(255) NOT NULL, lft INT NOT NULL, lvl INT NOT NULL, rgt INT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_64C19C1989D9B62 (slug), INDEX IDX_64C19C179066886 (root_id), INDEX IDX_64C19C1727ACA70 (parent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE article_comment ADD CONSTRAINT FK_79A616DBA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
@@ -52,5 +75,45 @@ class Version20170215164952 extends AbstractMigration
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE article');
         $this->addSql('DROP TABLE category');
+    }
+
+    /**
+     * Exécuté après la création de la base :
+     *     - on ajoute des utilisateurs
+     *     - on ajoute une catégorie
+     *
+     * @param Schema $schema
+     * @return void
+     */
+    public function postUp(Schema $schema)
+    {
+        $entityManager = $this->container->get('doctrine.orm.entity_manager');
+
+        //création d'une catégorie "root"
+        $rootCategory = new Category();
+        $rootCategory->setTitle('root');
+
+        //création de l'utilisateur admin
+        $adminUser = new User();
+        $adminUser->setEmail('admin@admin.com');
+        $adminUser->setPlainPassword('password');
+        $adminUser->setRoles(array(User::ROLE_ADMIN));
+
+        //création de l'utilisateur de test
+        $testUser = new User();
+        $testUser->setEmail('test@test.com');
+        $testUser->setPlainPassword('password');
+        $testUser->setRoles(array(User::ROLE_MEMBRE));
+
+        //enregistrement des connées en base
+        $entityManager->persist($rootCategory);
+        $entityManager->persist($adminUser);
+        $entityManager->persist($testUser);
+        $entityManager->flush();
+
+        //affiche qqu messages
+        $this->warnIf(true, 'The administrator user has been added with "admin@admin.com / password" => change this password !');
+        $this->warnIf(true, 'A test user has been added with "test@test.com / password"');
+        $this->warnIf(true, 'A root category has been added => rename/change it');
     }
 }
