958ad26e35026403bc5ed40959b478ac5436b112
flo130 <flo130@hotmail.com>
2017-04-21 10:44:15 +0200

    Update install.sh
    env prod

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 #quitte ce script si une erreur arrive
 set -e
 
-#récupération des sources 
+export SYMFONY_ENV=prod
+
+#rÃ©cupÃ©ration des sources 
 git clone https://github.com/flo130/florent-le-borgne.git
 cd florent-le-borgne
 
@@ -12,27 +14,27 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
-#installation des dépendances du projet
-php composer.phar install --no-dev --optimize-autoloader
+#installation des dÃ©pendances du projet
+composer install --no-dev --optimize-autoloader
 
 #suppression du fichier Composer
 rm composer.phar
 
-#changement des droits sur les répertoires de cache et de logs
+#changement des droits sur les rÃ©pertoires de cache et de logs
 HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
 sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
 sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
 
-#vérifie si tout est ok pour faire tourner Symfony
+#vÃ©rifie si tout est ok pour faire tourner Symfony
 php bin/symfony_requirements
 
-#vérifie si les basic de la sécurité sont ok
+#vÃ©rifie si les basic de la sÃ©curitÃ© sont ok
 php bin/console security:check
 
-#création de la base de données
+#crÃ©ation de la base de donnÃ©es
 php bin/console doctrine:database:create
 
-#installation du nouveau schéma
+#installation du nouveau schÃ©ma
 php bin/console doctrine:migrations:migrate --no-interaction
 
 #installation des assets
