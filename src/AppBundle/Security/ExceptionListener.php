4c68e45399a0f8c76bbe74b28380abc4576b556c
Florent Le Borgne <florent.leborgne@ouest-france.fr>
2017-12-14 14:54:11 +0100

    test

--- src/AppBundle/Security/ExceptionListener.php
+++ src/AppBundle/Security/ExceptionListener.php
@@ -12,11 +12,11 @@ class ExceptionListener extends BaseExceptionListener
 	 */
     protected function setTargetPath(Request $request)
     {
-        //cette methode permet de conserver une url demander pour la réutiliser 
+        //cette methode permet de conserver une url demandée pour la réutiliser
         //si jamais une action a empeché le chargement de la page
 
         //ex : je tente d'accéder à une page ou il faut etre authentifié, mais je ne le suis pas
-        //     alors on me redirige vers le form de login et suite à mon login, je suis redirigé vers cette derniere page
+        //     alors on me redirige vers le form de login et suite à la connexion, je suis redirigé vers cette derniere page
 
         //ici on dit que si on est en ajax, on va ignorer le comportement par 
         //défaut de Symfony : il faudra cabler le comportement dans le controlleur
